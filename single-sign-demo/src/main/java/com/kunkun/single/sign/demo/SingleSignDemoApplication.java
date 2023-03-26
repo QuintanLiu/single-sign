6167ea4d51bf9004a789b82416e49fa8a135551b
liukun <718526027@163.com>
2023-03-26 17:24:15 +0800

    single-sign init

---
+++ single-sign-demo/src/main/java/com/kunkun/single/sign/demo/SingleSignDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kunkun.single.sign.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSignDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SingleSignDemoApplication.class, args);
+  }
+
+}
---
+++ single-sign-demo/src/test/java/com/kunkun/single/sign/demo/SingleSignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunkun.single.sign.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSignDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
